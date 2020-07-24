fd88070aab0528fc6aca1aec48a10ebc4f4f4220
Tran Duc Nhien <ducnhientran071094@gmail.com>
2020-07-24 09:26:53 +0700

    dialog

--- src/TestPages/DialogExample.jsx
+++ src/TestPages/DialogExample.jsx
@@ -51,17 +51,17 @@ class DialogExample extends Component {
                         <DialogContent dividers>
                             <input type="text" value={this.state.inputField}     onChange={this.handleChange} />
                             <Typography gutterBottom>
-                            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-                            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                                in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
                             </Typography>
                             <Typography gutterBottom>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-                            lacus vel augue laoreet rutrum faucibus dolor auctor.
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                                lacus vel augue laoreet rutrum faucibus dolor auctor.
                             </Typography>
                             <Typography gutterBottom>
-                            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-                            scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-                            auctor fringilla.
+                                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
+                                scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
+                                auctor fringilla.
                             </Typography>
                         </DialogContent>
                         <DialogActions>
